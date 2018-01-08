995a91c285dbb2b777cebef71cd7658962573dcb
hack3r <hack3r@amshel>
2018-01-08 13:06:46 +0300

    removed veronicas picture, Added bullets for list

---
+++ about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<?php
+include("include/header.php");
+?>
+<!-- Main -->
+<div id="main-wrapper">
+    <div class="container" id="myContainer">
+        <div id="content">
+            <!-- Content -->
+            <article class="about animated bounceInRight">
+
+                <h2>BACKGROUND</h2>
+
+                <p>Mountain Top Safaris is an indigenous company registered in Kenya in 2014. The company has grown
+                    over the years and has managed to combine the strong points of big and small companies to give
+                    better and efficient services. We have managed to lease a network of customers within and outside
+                    the country. We are located at Karen Shopping Centre, Tana House 1st Floor. With the growing
+                    population of expatriates, tourists and shoppers, Karen is becoming one of the local hubs for
+                    Tourism. Not forgetting it has a history for itself like Karen Blixen museum, Karen Country and
+                    golf club and many other historical sites dating back early 20th Century.</p>
+
+                <p>Our processes are driven by world class practices across all service areas which enable us to provide
+                    the greatest value at the lowest risk. We pride ourselves as a specialized and focused provider
+                    of the best tourist destinations to our customers.</p>
+            </article>
+            <div>
+                <article class="about animated bounceInLeft">
+                    <h2>MISSION STATEMENT</h2>
+                    <ul style="list-style-type: circle; list-style-position: inside">
+                        <li>To be a model tourist provider within the region.</li>
+                        <li>Constantly offering quality services to our clients.</li>
+                        <li>Encourage team work and positive contribution.</li>
+                        <li>Practice fair competition.</li>
+                        <li>The company practices the best solutions for customer satisfaction.</li>
+                        <li>Conduct business with integrity and honesty.</li>
+                    </ul>
+                </article>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+</div>
+<!-- Footer -->
+<?php
+include("include/footer.php");
