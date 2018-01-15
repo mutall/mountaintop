ee7ff5be105eb9fbcc98352f6632134485d264f4
hack3r <hack3r@amshel>
2018-01-15 12:28:59 +0300

    format the mobile numbers

--- include/footer.php
+++ include/footer.php
@@ -6,7 +6,10 @@
                 <ul>
                     <li><i fa fa-map-marker></i>
                         <address>P.O Box 195-00502<br>Karen, Nairobi</address></li>
-                    <li><i fa fa-phone></i>+254716479022<br> +254722422434<br>+254750635435<br>+254729654116</li>
+                    <li><i fa fa-phone></i>+254 716-479-022<br> 
+                                            +254 722-422-434<br>
+                                            +254 750-635-435<br>
+                                            +254 729-654-116</li>
                     <li><i fa fa-envelope-o></i>info@mountaintopsafarisadventures.co.ke
                     </li>
                 </ul>
