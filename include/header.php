980e2cccb3e1e10e53bbf1962967fbfee053e8fd
hack3r <hack3r@amshel>
2018-01-31 11:21:09 +0300

    add lightbox include and ref files

--- include/header.php
+++ include/header.php
@@ -18,6 +18,9 @@
                 width: 100%;
                 height: auto;
             }
+            #social{
+                float:right;
+            }
         </style>
     </head>
 
@@ -28,18 +31,24 @@
                 <header id="header" class="container">
                     <!-- Logo -->
                     <div id="logo">
-                        <img src="images/new_logo.jpg">
+                        <img src="new_logo.jpg">
                     </div>
                     <!-- Nav -->
                     <nav id="navi">
                         <ul>
                             <li><a href="index.php">HOME</a></li>
-                            <li><a href="booking.php?form=">BOOK US</a></li>
+                            <li><a href="about.php">ABOUT US</a></li>
                             <li><a href="tours.php">TOURS</a></li>
                             <li><a href="gallery.php">GALLERY</a></li>
+                            <li><a href="booking.php?form=">BOOK US</a></li>
                             <li><a href="contact.php">CONTACT US</a></li>
-                            <li><a href="no-sidebar.html">ABOUT US</a></li>
                         </ul>
                     </nav>
+                    <div id="social">
+                    <i class="fa fa-facebook-square fa-3x" aria-hidden="true" ></i>
+                    <i class="fa fa-google-plus-square fa-3x" aria-hidden="true" ></i>
+                    <i class="fa fa-instagram fa-3x" aria-hidden="true" ></i>
+                    <i class="fa fa-youtube-square fa-3x" aria-hidden="true" ></i>
+                    </div>
                 </header>
             </div>
